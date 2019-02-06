30e269229f2488b65c32ef405f0df5b82266f952
varffs <patrickbest@Patricks-MacBook-Pro.local>
2019-02-06 21:36:01 +0000

    Scaffold: About section w/ basic styling

---
+++ partials/about.php
@@ -0,0 +1,38 @@
+<section id="about" class="container margin-top-mid margin-bottom-mid">
+  <div class="grid-row">
+
+    <div class="grid-item item-s-12 margin-bottom-basic">
+      <hr />
+      <hr />
+    </div>
+
+    <div class="grid-item item-s-12 item-m-7">
+      <h3 class="margin-bottom-basic">About</h3>
+
+      <div class="font-size-mid text-color-red">
+        <p>Fran Marchesi is a London based Senior Designer. She started her career as an illustrator specialising in hand-lettering before moving into branding. In the following eight years she has worked both in agencies and as an independent for clients all around the globe.</p>
+        <p>She has worked with a range of clients from startups and charities to eco-innovation enterprises, international universities, global financial institutions and media companies.</p>
+      </div>
+    </div>
+
+    <div class="grid-item item-s-12 item-m-5">
+      <h3 class="margin-bottom-basic">Get in touch</h3>
+
+      <ul class="font-size-mid text-color-red margin-bottom-basic">
+        <li>franmarchesi@gmail.com</li>
+        <li>+44 7875 947241</li>
+      </ul>
+
+      <ul class="font-size-mid text-color-red">
+        <li>Instagram &rarr;</li>
+        <li>Twitter &rarr;</li>
+        <li>Linkedin &rarr;</li>
+      </ul>
+    </div>
+
+    <div class="grid-item item-s-12">
+      <hr />
+    </div>
+
+  </div>
+</section>
