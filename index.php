1462db5150d7808f02f01f3af5567c38a02a1825
varffs <patrickbest@Patricks-MacBook-Pro.local>
2019-02-18 20:00:51 +0000

    scaffold layout with styles

--- index.php
+++ index.php
@@ -1,11 +1,99 @@
 <?php
 get_header();
+
+function renderGridContent($title) {
+?>
+<div class="home-item-content text-align-center">
+  <h4 class="text-color-red">2019</h4>
+  <h3 class="font-sans font-size-large"><?php echo $title; ?></h3>
+  <h4 class="font-style-italic">Brand Identity</h4>
+</div>
+<?php
+}
+
 ?>
 
 <main id="main-content">
   <section id="posts">
+
+    <div class="container">
+      <div class="grid-row margin-bottom-large">
+        <div class="grid-item item-s-12">
+
+          <!-- Row 1 -->
+          <div class="grid-row">
+            <div class="grid-item no-gutter item-s-12 item-m-6">
+              <div class="grid-row">
+                <div class="home-item grid-item no-gutter item-s-12 item-m-6">
+                  <?php renderGridContent('AROKIYAM'); ?>
+                </div>
+                <div class="home-item grid-item no-gutter item-s-12 item-m-6">
+                  <?php renderGridContent('RA250'); ?>
+                </div>
+                <div class="home-item grid-item no-gutter item-s-12">
+                  <?php renderGridContent('MARTIN SENYSZAK'); ?>
+                  <h3></h3>
+                </div>
+              </div>
+            </div>
+
+            <div class="home-item grid-item no-gutter item-s-12 item-m-6">
+              <?php renderGridContent('JAMM STUDIOS'); ?>
+            </div>
+          </div>
+
+          <!-- Row 2 -->
+          <div class="grid-row">
+            <div class="home-item grid-item no-gutter item-s-12 item-m-9">
+              <?php renderGridContent('ROMANCE FM'); ?>
+            </div>
+
+            <div class="home-item grid-item no-gutter item-s-12 item-m-3">
+              <?php renderGridContent('HARRIET HORTON'); ?>
+            </div>
+          </div>
+
+          <!-- Row 3 -->
+          <div class="grid-row">
+            <div class="home-item grid-item no-gutter item-s-12 item-m-6">
+              <?php renderGridContent('ELLE MAGAZINE'); ?>
+            </div>
+
+            <div class="grid-item no-gutter item-s-12 item-m-6">
+              <div class="grid-row">
+                <div class="home-item grid-item no-gutter item-s-12">
+                  <?php renderGridContent('WORLDPAY'); ?>
+                </div>
+                <div class="home-item grid-item no-gutter item-s-12">
+                  <?php renderGridContent('RAPHA'); ?>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Row 4 -->
+          <div class="grid-row">
+            <div class="home-item grid-item no-gutter item-s-12 item-m-4">
+              <?php renderGridContent('FUNHOUSE'); ?>
+            </div>
+
+            <div class="home-item grid-item no-gutter item-s-12 item-m-4">
+              <?php renderGridContent('AUDIO NETWORK'); ?>
+            </div>
+
+            <div class="home-item grid-item no-gutter item-s-12 item-m-4">
+              <?php renderGridContent('NIKE iD'); ?>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
     <div class="container">
       <div class="grid-row">
+        <div class="grid-item item-s-12">
+          <div class="grid-row">
 
 <?php
 if (have_posts()) {
@@ -13,8 +101,8 @@ if (have_posts()) {
     the_post();
 ?>
 
-        <article <?php post_class('grid-item item-s-12 item-m-4'); ?> id="post-<?php the_ID(); ?>">
-          <div class="grid-post">
+        <article <?php post_class('grid-item no-gutter item-s-12 item-m-4'); ?> id="post-<?php the_ID(); ?>">
+          <div class="home-item">
             <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
           </div>
         </article>
@@ -26,7 +114,8 @@ if (have_posts()) {
         <article class="u-alert grid-item item-s-12"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
 <?php
 } ?>
-
+          </div>
+        </div>
       </div>
     </div>
   </section>
